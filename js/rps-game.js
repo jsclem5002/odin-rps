024877701e39d3d25bfd4ec31c38fb4965efbd08
jsclem5002 <18202458+jsclem5002@users.noreply.github.com>
2025-06-12 11:27:39 -0400

    Added main game round loop logic, refactored round logic and score variables into main playGame function, added descriptive function comments.

--- js/rps-game.js
+++ js/rps-game.js
@@ -1,6 +1,62 @@
-let humanScore = 0;
-let computerScore = 0;
+let playGame = () => {
+  //Initialize scores
+  let humanScore = 0;
+  let computerScore = 0;
 
+  //Rock, paper, scissors logic
+  let playRound = (humanChoice, computerChoice) => {
+    humanChoice.toLowerCase();
+    return humanChoice === "rock" && computerChoice === "paper"
+      ? (console.log("You lose! Paper beats rock!"), computerScore++)
+      : humanChoice === "rock" && computerChoice === "scissors"
+      ? (console.log("You win! Rock beats scissors!"), humanScore++)
+      : humanChoice === "paper" && computerChoice === "scissors"
+      ? (console.log("You lose! Scissors beats paper!"), computerScore++)
+      : humanChoice === "paper" && computerChoice === "rock"
+      ? (console.log("You win! Paper beats rock!"), humanScore++)
+      : humanChoice === "scissor" && computerChoice === "rock"
+      ? (console.log("You lose! Rock beats scissors!"), computerScore++)
+      : humanChoice === "scissor" && computerChoice === "paper"
+      ? (console.log("You win! Scissor beats paper!"), humanScore++)
+      : console.log("It's a tie!");
+  };
+
+  //Rounds loop - change amount of rounds by adjusting the loop value
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(
+      "Human Score:" + humanScore + " | Computer Score: " + computerScore
+    );
+  }
+
+  //Return final score
+  return humanScore > computerScore
+    ? console.log(
+        "You Win! Final Score - Human Score: " +
+          humanScore +
+          " | Computer Score: " +
+          computerScore
+      )
+    : humanScore < computerScore
+    ? console.log(
+        "You Lose! Final Score - Human Score: " +
+          humanScore +
+          " | Computer Score: " +
+          computerScore
+      )
+    : console.log(
+        "It's a Tie! Final Score - Human Score: " +
+          humanScore +
+          " | Computer Score: " +
+          computerScore
+      );
+};
+
+//Auxillary Functions
+
+//Generate computer answer
 let getComputerChoice = () => {
   let computerChoice = Math.floor(Math.random() * 100) + 1;
   console.log(computerChoice);
@@ -11,33 +67,12 @@ let getComputerChoice = () => {
     : "scissors";
 };
 
+//Prompt user choice
 let getHumanChoice = () => {
   let humanChoice = prompt("Rock, Paper, or Scissors?", "");
   console.log(humanChoice);
   return humanChoice;
 };
 
-let playRound = (humanChoice, computerChoice) => {
-  humanChoice.toLowerCase();
-  return humanChoice === "rock" && computerChoice === "paper"
-    ? (console.log("You lose! Paper beats rock!"), computerScore++)
-    : humanChoice === "rock" && computerChoice === "scissors"
-    ? (console.log("You win! Rock beats scissors!"), humanScore++)
-    : humanChoice === "paper" && computerChoice === "scissors"
-    ? (console.log("You lose! Scissors beats paper!"), computerScore++)
-    : humanChoice === "paper" && computerChoice === "rock"
-    ? (console.log("You win! Paper beats rock!"), humanScore++)
-    : humanChoice === "scissor" && computerChoice === "rock"
-    ? (console.log("You lose! Rock beats scissors!"), computerScore++)
-    : humanChoice === "scissor" && computerChoice === "paper"
-    ? (console.log("You win! Scissor beats paper!"), humanScore++)
-    : console.log("It's a tie!");
-};
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-console.log(
-  "Human Score:" + humanScore + " | Computer Score: " + computerScore
-);
+//Main game function call
+playGame();
