5c0e55e030cf31d86e5d3d836f1254963f9a6156
jsclem5002 <18202458+jsclem5002@users.noreply.github.com>
2025-06-12 11:06:13 -0400

    Added rock, paper, scissors game logic function

--- js/rps-game.js
+++ js/rps-game.js
@@ -1,11 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
   let computerChoice = Math.floor(Math.random() * 100) + 1;
   console.log(computerChoice);
   return computerChoice > 66
-    ? "Rock"
+    ? "rock"
     : computerChoice < 33
-    ? "Paper"
-    : "Scissors";
+    ? "paper"
+    : "scissors";
 };
 
 let getHumanChoice = () => {
@@ -14,5 +17,27 @@ let getHumanChoice = () => {
   return humanChoice;
 };
 
-getComputerChoice();
-getHumanChoice();
+let playRound = (humanChoice, computerChoice) => {
+  humanChoice.toLowerCase();
+  return humanChoice === "rock" && computerChoice === "paper"
+    ? (console.log("You lose! Paper beats rock!"), computerScore++)
+    : humanChoice === "rock" && computerChoice === "scissors"
+    ? (console.log("You win! Rock beats scissors!"), humanScore++)
+    : humanChoice === "paper" && computerChoice === "scissors"
+    ? (console.log("You lose! Scissors beats paper!"), computerScore++)
+    : humanChoice === "paper" && computerChoice === "rock"
+    ? (console.log("You win! Paper beats rock!"), humanScore++)
+    : humanChoice === "scissor" && computerChoice === "rock"
+    ? (console.log("You lose! Rock beats scissors!"), computerScore++)
+    : humanChoice === "scissor" && computerChoice === "paper"
+    ? (console.log("You win! Scissor beats paper!"), humanScore++)
+    : console.log("It's a tie!");
+};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(
+  "Human Score:" + humanScore + " | Computer Score: " + computerScore
+);
